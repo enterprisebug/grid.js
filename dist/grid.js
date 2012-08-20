f357bfb49b4ab5d084be8a2370b73885185cc36d
Daniel Sirz <daniel.sirz@gmail.com>
2012-08-20 06:28:17 +0200

    Added color picker to UI

--- dist/grid.js
+++ dist/grid.js
@@ -1,8 +1,6 @@
 (function(window, document, undefined) {
-	/**
-	 * Fills in default values.
-	 */
-	function merge(obj) {
+
+	function mergeWithDefaultValues(obj) {
 		for (var i = 1; i < arguments.length; i++) {
 			var def = arguments[i];
 			for (var n in def) {
@@ -13,22 +11,22 @@
 		}
 		return obj;
 	}
-	
+
 	var defaults = {
 		distance : 10,
 		lineWidth : 0.5,
-		color : "black",
+		gridColor : "#000000",
 		caption : true
 	};
 	
 	/** The constructor */
 	var Grid = function Grid(o) {
 		if (!this.draw) return new Grid(o);
-		this.opts = merge(o || {}, Grid.defaults, defaults);
+		this.opts = mergeWithDefaultValues(o || {}, Grid.defaults, defaults);
 	};
 	
 	Grid.defaults = {};
-	merge(Grid.prototype, {
+	mergeWithDefaultValues(Grid.prototype, {
 		draw: function(target) {
 			var self = this;
 			var o = self.opts;
@@ -37,20 +35,20 @@
 				target.save();
 
 				target.lineWidth = o.lineWidth;
-				target.strokeStyle = o.color;
+				target.strokeStyle = o.gridColor;
 				
-				for (var i = 0; i <= target.canvas.width; i = i + o.distance) {
+				for (var y = 0; y <= target.canvas.width; y += o.distance) {
 					target.beginPath();
-					target.moveTo(0, i);
-					target.lineTo(target.canvas.width, i);
+					target.moveTo(0, y);
+					target.lineTo(target.canvas.width, y);
 					target.closePath();
 					target.stroke();
 				}
 				
-				for (var j = 0; j <= target.canvas.width; j = j + o.distance) {
+				for (var x = 0; x <= target.canvas.width; x += o.distance) {
 					target.beginPath();
-					target.moveTo(j, 0);
-					target.lineTo(j, target.canvas.height);
+					target.moveTo(x, 0);
+					target.lineTo(x, target.canvas.height);
 					target.closePath();
 					target.stroke();
 				}
